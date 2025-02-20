8d0da1ced93649700e6751dda672ea67a56754e5
farhan <farhan.khan@arbisoft.com>
2025-02-20 14:39:36 +0500

    chore: test branch

--- xblocks_contrib/poll/poll.py
+++ xblocks_contrib/poll/poll.py
@@ -1,12 +1,24 @@
-"""TO-DO: Write a description of what this XBlock is."""
+"""Poll block is ungraded xmodule used by students to
+to do set of polls.
 
-from importlib.resources import files
+On the client side we show:
+If student does not yet anwered - Question with set of choices.
+If student have answered - Question with statistics for each answers.
+"""
+import html
+import json
+from collections import OrderedDict
+from copy import deepcopy
 
-from django.utils import translation
+import markupsafe
+from django.utils.translation import gettext_noop as _
+from lxml import etree
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
-from xblock.fields import Integer, Scope
+from xblock.fields import Boolean, Dict, List, Scope, String
 from xblock.utils.resources import ResourceLoader
+
+from xblocks_contrib.utils.stringify import stringify_children
 
 resource_loader = ResourceLoader(__name__)
 
@@ -14,68 +26,172 @@ resource_loader = ResourceLoader(__name__)
 # This Xblock is just to test the strucutre of xblocks-contrib
 @XBlock.needs("i18n")
 class PollBlock(XBlock):
-    """
-    TO-DO: document what your XBlock does.
-    """
-
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
-
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0,
-        scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
+    """Poll Block"""
 
     # Indicates that this XBlock has been extracted from edx-platform.
     is_extracted = True
 
-    def resource_string(self, path):
-        """Handy helper for getting resources from our kit."""
-        return files(__package__).joinpath(path).read_text(encoding="utf-8")
+    # Name of poll to use in links to this poll
+    display_name = String(
+        help=_("The display name for this component."),
+        scope=Scope.settings
+    )
 
-    # TO-DO: change this view to display your data your own way.
-    def student_view(self, context=None):
-        """
-        Create primary view of the XBlock, shown to students when viewing courses.
-        """
-        if context:
-            pass  # TO-DO: do something based on the context.
+    voted = Boolean(
+        help=_("Whether this student has voted on the poll"),
+        scope=Scope.user_state,
+        default=False
+    )
+    poll_answer = String(
+        help=_("Student answer"),
+        scope=Scope.user_state,
+        default=''
+    )
+    poll_answers = Dict(
+        help=_("Poll answers from all students"),
+        scope=Scope.user_state_summary
+    )
 
+    # List of answers, in the form {'id': 'some id', 'text': 'the answer text'}
+    answers = List(
+        help=_("Poll answers from xml"),
+        scope=Scope.content,
+        default=[]
+    )
+
+    question = String(
+        help=_("Poll question"),
+        scope=Scope.content,
+        default=''
+    )
+
+    resources_dir = None
+    uses_xmodule_styles_setup = True
+
+    def student_view(self, _context):
+        """
+        Renders the student view.
+        """
+        # import pdb; pdb.set_trace()
         frag = Fragment()
-        frag.add_content(
-            resource_loader.render_django_template(
-                "templates/poll.html",
-                {
-                    "count": self.count,
-                },
-                i18n_service=self.runtime.service(self, "i18n"),
-            )
-        )
-
-        frag.add_css(self.resource_string("static/css/poll.css"))
-        frag.add_javascript(self.resource_string("static/js/src/poll.js"))
-        frag.initialize_js("PollBlock")
+        frag.add_content(resource_loader.render_django_template(
+            "templates/poll.html", {
+                'element_id': self.scope_ids.usage_id.html_id(),
+                'element_class': self.scope_ids.usage_id.block_type,
+                'configuration_json': self.dump_poll(),
+            },
+            i18n_service=self.runtime.service(self, 'i18n')
+        ))
+        frag.add_css(resource_loader.load_unicode("static/css/poll.css"))
+        frag.add_javascript(resource_loader.load_unicode("static/js/src/poll.js"))
+        frag.initialize_js('PollBlock')
         return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=""):
-        """
-        Increments data. An example handler.
-        """
-        if suffix:
-            pass  # TO-DO: Use the suffix when storing data.
-        # Just to show data coming in...
-        assert data["hello"] == "world"
+    def dump_poll(self):
+        """Dump poll information.
 
-        self.count += 1
-        return {"count": self.count}
+        Returns:
+            string - Serialize json.
+        """
+        # FIXME: hack for resolving caching `default={}` during definition
+        # poll_answers field
+        if self.poll_answers is None:
+            self.poll_answers = {}
 
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
+        answers_to_json = OrderedDict()
+
+        # FIXME: fix this, when xblock support mutable types.
+        # Now we use this hack.
+        temp_poll_answers = self.poll_answers
+
+        # Fill self.poll_answers, prepare data for template context.
+        for answer in self.answers:
+            # Set default count for answer = 0.
+            if answer['id'] not in temp_poll_answers:
+                temp_poll_answers[answer['id']] = 0
+            answers_to_json[answer['id']] = html.escape(answer['text'], quote=False)
+        self.poll_answers = temp_poll_answers
+
+        return json.dumps({
+            'answers': answers_to_json,
+            'question': html.escape(self.question, quote=False),
+            # to show answered poll after reload:
+            'poll_answer': self.poll_answer,
+            'poll_answers': self.poll_answers if self.voted else {},
+            'total': sum(self.poll_answers.values()) if self.voted else 0,
+            # TODO: Uncomment it
+            # 'reset': str(self.xml_attributes.get('reset', 'true')).lower()
+        })
+
+    _tag_name = 'poll_question'
+    _child_tag_name = 'answer'
+    #
+    # @classmethod
+    # def definition_from_xml(cls, xml_object, system):
+    #     """Pull out the data into dictionary.
+    #
+    #     Args:
+    #         xml_object: xml from file.
+    #         system: `system` object.
+    #
+    #     Returns:
+    #         (definition, children) - tuple
+    #         definition - dict:
+    #             {
+    #                 'answers': <List of answers>,
+    #                 'question': <Question string>
+    #             }
+    #     """
+    #     # Check for presense of required tags in xml.
+    #     if len(xml_object.xpath(cls._child_tag_name)) == 0:
+    #         raise ValueError("Poll_question definition must include \
+    #             at least one 'answer' tag")
+    #
+    #     xml_object_copy = deepcopy(xml_object)
+    #     answers = []
+    #     for element_answer in xml_object_copy.findall(cls._child_tag_name):
+    #         answer_id = element_answer.get('id', None)
+    #         if answer_id:
+    #             answers.append({
+    #                 'id': answer_id,
+    #                 'text': stringify_children(element_answer)
+    #             })
+    #         xml_object_copy.remove(element_answer)
+    #
+    #     definition = {
+    #         'answers': answers,
+    #         'question': stringify_children(xml_object_copy)
+    #     }
+    #     children = []
+    #
+    #     return (definition, children)
+    #
+    # def definition_to_xml(self, resource_fs):
+    #     """Return an xml element representing to this definition."""
+    #     poll_str = markupsafe.Markup('<{tag_name}>{text}</{tag_name}>').format(
+    #         tag_name=self._tag_name, text=self.question)
+    #     xml_object = etree.fromstring(poll_str)
+    #     xml_object.set('display_name', self.display_name)
+    #
+    #     def add_child(xml_obj, answer):  # lint-amnesty, pylint: disable=unused-argument
+    #         # Escape answer text before adding to xml tree.
+    #         answer_text = str(answer['text'])
+    #         child_str = markupsafe.escape('{tag_begin}{text}{tag_end}').format(
+    #             tag_begin=markupsafe.Markup('<{tag_name} id="{id}">').format(
+    #                 tag_name=self._child_tag_name,
+    #                 id=answer['id']
+    #             ),
+    #             text=answer_text,
+    #             tag_end=markupsafe.Markup('</{tag_name}>').format(tag_name=self._child_tag_name)
+    #         )
+    #         child_node = etree.fromstring(child_str)
+    #         xml_object.append(child_node)
+    #
+    #     for answer in self.answers:
+    #         add_child(xml_object, answer)
+    #
+    #     return xml_object
+
     @staticmethod
     def workbench_scenarios():
         """Create canned scenario for display in the workbench."""
@@ -96,9 +212,49 @@ class PollBlock(XBlock):
             ),
         ]
 
-    @staticmethod
-    def get_dummy():
+    @XBlock.json_handler
+    def handle_reset_state(self):
+        pass
+
+    def handle_ajax(self, dispatch, data):  # lint-amnesty, pylint: disable=unused-argument
+        """Ajax handler.
+
+        Args:
+            dispatch: string request slug
+            data: dict request data parameters
+
+        Returns:
+            json string
         """
-        Generate initial i18n with dummy method.
-        """
-        return translation.gettext_noop("Dummy")
+        if dispatch in self.poll_answers and not self.voted:
+            # FIXME: fix this, when xblock will support mutable types.
+            # Now we use this hack.
+            temp_poll_answers = self.poll_answers
+            temp_poll_answers[dispatch] += 1
+            self.poll_answers = temp_poll_answers
+
+            self.voted = True
+            self.poll_answer = dispatch
+            return json.dumps({'poll_answers': self.poll_answers,
+                               'total': sum(self.poll_answers.values()),
+                               'callback': {'objectName': 'Conditional'}
+                               })
+        elif dispatch == 'get_state':
+            return json.dumps({'poll_answer': self.poll_answer,
+                               'poll_answers': self.poll_answers,
+                               'total': sum(self.poll_answers.values())
+                               })
+        elif dispatch == 'reset_poll' and self.voted and \
+            self.xml_attributes.get('reset', 'True').lower() != 'false':
+            self.voted = False
+
+            # FIXME: fix this, when xblock will support mutable types.
+            # Now we use this hack.
+            temp_poll_answers = self.poll_answers
+            temp_poll_answers[self.poll_answer] -= 1
+            self.poll_answers = temp_poll_answers
+
+            self.poll_answer = ''
+            return json.dumps({'status': 'success'})
+        else:  # return error message
+            return json.dumps({'error': 'Unknown Command!'})
--- xblocks_contrib/poll/static/js/src/poll.js
+++ xblocks_contrib/poll/static/js/src/poll.js
@@ -1,38 +1,317 @@
 
 /* JavaScript for PollBlock. */
+var _this
+const blockIdentifier = '._poll_question_extracted'
+
 function PollBlock(runtime, element) {
-    const updateCount = (result) => {
-        $('.count', element).text(result.count);
-    };
 
-    const handlerUrl = runtime.handlerUrl(element, 'increment_count');
+    const questionEl = $(element).find(blockIdentifier);
+    if (questionEl.length !== 1) {
+        // We require one question DOM element.
+        console.log('ERROR: PollMain constructor requires one question DOM element.');
 
-    $('p', element).on('click', (eventObject) => {
+        return;
+    }
+
+    // Just a safety precussion. If we run this code more than once, multiple 'click' callback handlers will be
+    // attached to the same DOM elements. We don't want this to happen.
+    if (questionEl.attr('poll_main_processed') === 'true') {
+        console.log(
+            'ERROR: PolMain JS constructor was called on a DOM element that has already been processed once.'
+        );
+
+        return;
+    }
+
+    // This element was not processed earlier.
+    // Make sure that next time we will not process this element a second time.
+    questionEl.attr('poll_main_processed', 'true');
+
+    // Access this object inside inner functions.
+    _this = this;
+
+    // DOM element which contains the current poll along with any conditionals. By default we assume that such
+    // element is not present. We will try to find it.
+    this.wrapperSectionEl = null;
+
+    (function(tempEl, c1) {
+        while (tempEl.tagName.toLowerCase() !== 'body') {
+            tempEl = $(tempEl).parent()[0];
+            c1 += 1;
+
+            if (
+                (tempEl.tagName.toLowerCase() === 'div')
+        && ($(tempEl).data('block-type') === 'wrapper')
+            ) {
+                _this.wrapperSectionEl = tempEl;
+
+                break;
+            } else if (c1 > 50) {
+                // In case something breaks, and we enter an endless loop, a sane
+                // limit for loop iterations.
+
+                break;
+            }
+        }
+    }($(element)[0], 0));
+
+    try {
+        this.jsonConfig = JSON.parse(questionEl.children('.poll_question_div').html());
+
         $.ajax({
             type: 'POST',
-            url: handlerUrl,
-            contentType: 'application/json',
-            data: JSON.stringify({hello: 'world'}),
-            success: updateCount
+            url: runtime.handleUrl(element, 'handle_get_state'),
+            data: JSON.stringify(null),
+            success: function(response) {
+                _this.jsonConfig.poll_answer = response.poll_answer;
+                _this.jsonConfig.total = response.total;
+
+                $.each(response.poll_answers, function(index, value) {
+                    _this.jsonConfig.poll_answers[index] = value;
+                });
+
+                // xss-lint: disable=javascript-jquery-html
+                questionEl.children('.poll_question_div').html(JSON.stringify(_this.jsonConfig));
+
+                _this.postInit(runtime);
+            }
+        });
+
+        return;
+    } catch (err) {
+        console.log(
+            'ERROR: Invalid JSON config for poll ID "' + this.id + '".',
+            'Error messsage: "' + err.message + '".'
+        );
+    }
+
+}
+
+function showAnswerGraph(poll_answers, total) {
+
+    var totalValue;
+    totalValue = parseFloat(total);
+    if (isFinite(totalValue) === false) {
+        return;
+    }
+
+    _this = this;
+
+    $.each(poll_answers, function(index, value) {
+        var numValue, percentValue;
+
+        numValue = parseFloat(value);
+        if (isFinite(numValue) === false) {
+            return;
+        }
+
+        percentValue = (numValue / totalValue) * 100.0;
+
+        _this.answersObj[index].statsEl.show();
+        // eslint-disable-next-line max-len
+        _this.answersObj[index].numberEl.html(HtmlUtils.HTML('' + value + ' (' + percentValue.toFixed(1) + '%)').toString());
+        _this.answersObj[index].percentEl.css({
+            width: '' + percentValue.toFixed(1) + '%'
         });
     });
+}
 
-    $(() => {
-        /*
-        Use `gettext` provided by django-statici18n for static translations
-        */
+function submitAnswer(runtime, answer, answerObj) {
 
-        // eslint-disable-next-line no-undef
-        const dummyText = gettext('Hello World');
+    // Make sure that the user can answer a question only once.
+    if (this.questionAnswered === true) {
+        return;
+    }
+    this.questionAnswered = true;
 
-        // Example usage of interpolation for translated strings
-        // eslint-disable-next-line no-undef
-        const message = StringUtils.interpolate(
-            gettext('You are enrolling in {courseName}'),
-            {
-                courseName: 'Rock & Roll 101'
+    _this = this;
+
+    answerObj.buttonEl.addClass('answered');
+
+    var data = {
+        answer: answer
+    }
+
+    // Send the data to the server as an AJAX request. Attach a callback that will
+    // be fired on server's response.
+    $.ajax({
+        type: 'POST',
+        url: runtime.handleUrl(answerObj, 'handle_submit_state'),
+        data: JSON.stringify(data),
+        success: function (response) {
+            console.log('success! response = ');
+            console.log(response);
+
+            _this.showAnswerGraph(response.poll_answers, response.total);
+
+            if (_this.canReset === true) {
+                _this.resetButton.show();
             }
-        );
-        console.log(message); // This is just for demonstration purposes
+
+            // Initialize Conditional constructors.
+            if (_this.wrapperSectionEl !== null) {
+                $(_this.wrapperSectionEl).find('.xmodule_ConditionalModule').each(function(index, value) {
+                    // eslint-disable-next-line no-new
+                    new window.Conditional(value, _this.runtime, _this.id.replace(/^poll_/, ''));
+                });
+            }
+        }
     });
+}
+
+function submitReset(runtime, element) {
+
+    _this = this;
+
+    console.log('submit reset');
+    const questionEl = $(element).find(blockIdentifier);
+    // Send the data to the server as an AJAX request. Attach a callback that will
+    // be fired on server's response.
+    $.ajax({
+        type: 'POST',
+        url: runtime.handleUrl(element, 'handle_reset_state'),
+        data: JSON.stringify({}),
+        success:  function(response) {
+            console.log('success! response = ');
+            console.log(response);
+
+            if ((response.hasOwnProperty('status') !== true) || (typeof response.status !== 'string')
+                || (response.status.toLowerCase() !== 'success')) {
+                return;
+            }
+
+            _this.questionAnswered = false;
+            questionEl.find('.button.answered').removeClass('answered');
+            questionEl.find('.stats').hide();
+            _this.resetButton.hide();
+
+            // Initialize Conditional constructors. We will specify the third parameter as 'true'
+            // notifying the constructor that this is a reset operation.
+            if (_this.wrapperSectionEl !== null) {
+                $(_this.wrapperSectionEl).find('.xmodule_ConditionalModule').each(function(index, value) {
+                    // eslint-disable-next-line no-new
+                    new window.Conditional(value, _this.runtime, _this.id.replace(/^poll_/, ''));
+                });
+            }
+        }
+    });
+}
+
+function postInit(runtime) {
+
+    // Access this object inside inner functions.
+    _this = this;
+    const questionEl = $(element).find(blockIdentifier);
+    if ((this.jsonConfig.poll_answer.length > 0) && (this.jsonConfig.answers.hasOwnProperty(this.jsonConfig.poll_answer) === false)
+    ) {
+        HtmlUtils.append(questionEl, HtmlUtils.joinHtml(
+            HtmlUtils.HTML('<h3>Error!</h3>'),
+            HtmlUtils.HTML(
+                '<p>XML data format changed. List of answers was modified, but poll data was not updated.</p>'
+            )
+        ));
+
+        return;
+    }
+
+    // Get the DOM id of the question.
+    this.id = questionEl.attr('id');
+
+    // Get the URL to which we will post the users answer to the question.
+    // this.ajax_url = questionEl.data('ajax-url');
+
+    this.questionHtmlMarkup = $('<div />').html(HtmlUtils.HTML(this.jsonConfig.question).toString()).text();
+    questionEl.append(HtmlUtils.HTML(this.questionHtmlMarkup).toString());
+
+    // When the user selects and answer, we will set this flag to true.
+    this.questionAnswered = false;
+
+    this.answersObj = {};
+    this.shortVersion = true;
+
+    $.each(this.jsonConfig.answers, function(index, value) {
+        if (value.length >= 18) {
+            _this.shortVersion = false;
+        }
+    });
+
+    $.each(this.jsonConfig.answers, function(index, value) {
+        var answer;
+
+        answer = {};
+
+        _this.answersObj[index] = answer;
+
+        answer.el = $('<div class="poll_answer"></div>');
+
+        answer.questionEl = $('<div class="question"></div>');
+        answer.buttonEl = $('<div class="button"></div>');
+        answer.textEl = $('<div class="text"></div>');
+        answer.questionEl.append(HtmlUtils.HTML(answer.buttonEl).toString());
+        answer.questionEl.append(HtmlUtils.HTML(answer.textEl).toString());
+
+        answer.el.append(HtmlUtils.HTML(answer.questionEl).toString());
+
+        answer.statsEl = $('<div class="stats"></div>');
+        answer.barEl = $('<div class="bar"></div>');
+        answer.percentEl = $('<div class="percent"></div>');
+        answer.barEl.append(HtmlUtils.HTML(answer.percentEl).toString());
+        answer.numberEl = $('<div class="number"></div>');
+        answer.statsEl.append(HtmlUtils.HTML(answer.barEl).toString());
+        answer.statsEl.append(HtmlUtils.HTML(answer.numberEl).toString());
+
+        answer.statsEl.hide();
+
+        answer.el.append(HtmlUtils.HTML(answer.statsEl).toString());
+
+        answer.textEl.html(HtmlUtils.HTML(value).toString());
+
+        if (_this.shortVersion === true) {
+            // eslint-disable-next-line no-shadow
+            $.each(answer, function(index, value) {
+                if (value instanceof jQuery) {
+                    value.addClass('short');
+                }
+            });
+        }
+
+        answer.el.appendTo(questionEl);
+
+        answer.textEl.on('click', function() {
+            _this.submitAnswer(runtime, index, answer);
+        });
+
+        answer.buttonEl.on('click', function() {
+            _this.submitAnswer(runtime, index, answer);
+        });
+
+        if (index === _this.jsonConfig.poll_answer) {
+            answer.buttonEl.addClass('answered');
+            _this.questionAnswered = true;
+        }
+    });
+
+    console.log(this.jsonConfig.reset);
+
+    if ((typeof this.jsonConfig.reset === 'string') && (this.jsonConfig.reset.toLowerCase() === 'true')) {
+        this.canReset = true;
+
+        this.resetButton = $('<div class="button reset-button">Change your vote</div>');
+
+        if (this.questionAnswered === false) {
+            this.resetButton.hide();
+        }
+
+        HtmlUtils.append(questionEl, this.resetButton);
+        this.resetButton.on('click', function() {
+            _this.submitReset();
+        });
+    } else {
+        this.canReset = false;
+    }
+
+    // If it turns out that the user already answered the question, show the answers graph.
+    if (this.questionAnswered === true) {
+        this.showAnswerGraph(this.jsonConfig.poll_answers, this.jsonConfig.total);
+    }
 }
