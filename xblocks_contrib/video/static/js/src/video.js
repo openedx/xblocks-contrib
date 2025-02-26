83ff6ae1d51e8c818cd44b227f088efc654585bd
farhan <farhan.khan@arbisoft.com>
2025-02-26 09:07:52 +0500

    stable 5, Adds django template html and some js

--- xblocks_contrib/video/static/js/src/video.js
+++ xblocks_contrib/video/static/js/src/video.js
@@ -1,5 +1,9 @@
 /* JavaScript for VideoBlock. */
 
 function VideoBlock(runtime, element) {
+  console.log('In VideoBlock.')
+  // var storage = VideoStorage('VideoState', id)
+  // console.log(storage)
+
   console.log('VideoBlock JS has been loaded.')
 }
