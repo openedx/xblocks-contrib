d402cedf88875cff327481fac551051bafdbb2b4
salman2013 <salman.nawaz@arbisoft.com>
2025-04-14 17:01:44 +0500

    fix: fix test case

--- xblocks_contrib/poll/poll.py
+++ xblocks_contrib/poll/poll.py
@@ -162,12 +162,7 @@ class PollBlock(XBlock):
             'total': sum(self.poll_answers.values())
         }
 
-    @XBlock.json_handler
-    def handle_submit_state(self, data, suffix=''):     # lint-amnesty, pylint: disable=unused-argument
-        """
-        handler to submit poll answer.
-        """
-        answer = data.get('answer')  # Extract the answer from the data payload
+    def submit_answer(self, answer):
         if not answer:
             return {'error': 'No answer provided!'}
 
@@ -186,6 +181,33 @@ class PollBlock(XBlock):
                 'callback': {'objectName': 'Conditional'}
             }
         return {"error": "Invalid answer or already voted."}
+
+
+    @XBlock.json_handler
+    def handle_submit_state(self, data, suffix=''):     # lint-amnesty, pylint: disable=unused-argument
+        """
+        handler to submit poll answer.
+        """
+        answer = data.get('answer')  # Extract the answer from the data payload
+        self.submit_answer(answer)
+        # if not answer:
+        #     return {'error': 'No answer provided!'}
+
+        # if answer in self.poll_answers and not self.voted:
+        #     # FIXME: fix this, when xblock will support mutable types.
+        #     # Now we use this hack.
+        #     temp_poll_answers = self.poll_answers
+        #     temp_poll_answers[answer] += 1
+        #     self.poll_answers = temp_poll_answers
+
+        #     self.voted = True
+        #     self.poll_answer = answer
+        #     return {
+        #         'poll_answers': self.poll_answers,
+        #         'total': sum(self.poll_answers.values()),
+        #         'callback': {'objectName': 'Conditional'}
+        #     }
+        # return {"error": "Invalid answer or already voted."}
 
     @XBlock.json_handler
     def handle_reset_state(self):
