b9d6c9ebec1ca39db57b0359857ae83991bf7893
farhan <farhan.khan@arbisoft.com>
2025-05-27 19:23:09 +0500

    chore: fix ++

--- xblocks_contrib/video/webpack.prod.config.js
+++ xblocks_contrib/video/webpack.prod.config.js
@@ -1,34 +1,9 @@
-const path = require('path');
-const webpack = require('webpack');
-const {WebpackManifestPlugin} = require('webpack-manifest-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const {merge} = require('webpack-merge');
+const _ = require('underscore');
+const commonConfig = require('./webpack.common.config.js');
 
-const config = {
-    mode: 'production',
-    entry: {
-        'video-xblock': path.resolve(process.cwd(), 'static/js/src/10_main.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'static/js/dist'),
-        filename: '[name].js',
-        clean: true,
-        publicPath: '/'
-    },
-    resolve: {
-        alias: {
-            'edx-ui-toolkit/js': 'edx-ui-toolkit/src/js',
-            'hls': 'hls.js/dist/hls.js'
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new WebpackManifestPlugin({
-            seed: {
-                base_url: '/static/js/dist',
-            },
-        })
+module.exports = commonConfig;
 
-    ]
-};
-
-module.exports = config
+module.exports = merge(commonConfig, {
+    mode: 'production'
+});
