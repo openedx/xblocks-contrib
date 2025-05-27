cbd38d4b674405fd1a2dd4400bb9ee224c853cf6
farhan <farhan.khan@arbisoft.com>
2025-05-27 18:36:12 +0500

    chore: separate out webpack config files

---
+++ xblocks_contrib/video/webpack.dev.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const {WebpackManifestPlugin} = require('webpack-manifest-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const config = {
+    mode: 'production',
+    entry: {
+        'video-xblock': path.resolve(process.cwd(), 'static/js/src/10_main.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'static/js/dist'),
+        filename: '[name].js',
+        clean: true,
+        publicPath: '/'
+    },
+    resolve: {
+        alias: {
+            'edx-ui-toolkit/js': 'edx-ui-toolkit/src/js',
+            'hls': 'hls.js/dist/hls.js'
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new WebpackManifestPlugin({
+            seed: {
+                base_url: '/static/js/dist',
+            },
+        })
+
+    ],
+    optimization: {
+       // TODO: Keep it enable for production, make separate config
+       minimize: false
+    }
+};
+
+module.exports = config
