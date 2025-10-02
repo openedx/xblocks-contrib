dd72c56ae7a61c934d37ab5a40cb7e4dff2790a5
salman2013 <salman.nawaz@arbisoft.com>
2025-10-02 16:13:31 +0500

    fix: load js file url

--- xblocks_contrib/video/webpack.common.config.js
+++ xblocks_contrib/video/webpack.common.config.js
@@ -7,7 +7,7 @@ const config = {
         'video-xblock': path.resolve(process.cwd(), 'static/js/src/10_main.js'),
     },
     output: {
-        path: path.resolve(__dirname, 'static/js/dist'),
+        path: path.resolve(__dirname, 'public/js'),
         filename: '[name].js',
         clean: true,
         publicPath: '/'
@@ -20,11 +20,6 @@ const config = {
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new WebpackManifestPlugin({
-            seed: {
-                base_url: '/static/js/dist',
-            },
-        })
     ]
 };
 
