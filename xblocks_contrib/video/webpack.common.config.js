de0c3d6a2ed33a560259276054affd647e59d720
farhan <farhan.khan@arbisoft.com>
2026-01-02 17:11:50 +0500

    feat: Adds the Video XBlock code extracted from the edx-platform

---
+++ xblocks_contrib/video/webpack.common.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const {WebpackManifestPlugin} = require('webpack-manifest-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const config = {
+    entry: {
+        'video-xblock': path.resolve(process.cwd(), 'static/js/src/10_main.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/js'),
+        filename: '[name].js',
+        clean: true,
+        publicPath: '/'
+    },
+    resolve: {
+        alias: {
+            'edx-ui-toolkit/js': 'edx-ui-toolkit/src/js',
+            'hls': 'hls.js/dist/hls.js'
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.JS_ENV_EXTRA_CONFIG': JSON.parse(process.env.JS_ENV_EXTRA_CONFIG || '{}'),
+            'CAPTIONS_CONTENT_TO_REPLACE': JSON.stringify(process.env.CAPTIONS_CONTENT_TO_REPLACE || ''),
+            'CAPTIONS_CONTENT_REPLACEMENT': JSON.stringify(process.env.CAPTIONS_CONTENT_REPLACEMENT || '')
+        })
+    ]
+};
+
+module.exports = config
