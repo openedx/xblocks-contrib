e193f2e01484eb402e3182ec6bffa7a2081c0686
farhan <farhan.khan@arbisoft.com>
2026-01-23 18:59:20 +0500

    fix: fix quality check for video block code

--- test_settings.py
+++ test_settings.py
@@ -5,6 +5,7 @@ Minimal Django settings for running tests on xblocks_contrib.
 INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "edxval",
 ]
 
 DATABASES = {
@@ -12,3 +13,5 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
     }
 }
+
+TRANSCRIPT_LANG_CACHE_TIMEOUT = 60 * 60 * 24  # 24 hours
