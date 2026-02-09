c52b8757313881b7bc5b9f4f789956726874b7f0
Irtaza Akram <irtaza.akram@arbisoft.com>
2026-02-09 19:30:32 +0500

    fix: add problem xblock

---
+++ xblocks_contrib/problem/assets/karma.conf.js
@@ -0,0 +1,120 @@
+/* eslint-env node */
+'use strict';
+
+const path = require('path');
+
+const uiToolkitRoot = path.dirname(require.resolve('edx-ui-toolkit/package.json'));
+
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+
+        frameworks: ['jasmine'],
+
+        files: [
+            { pattern: require.resolve('jquery/dist/jquery.js'), included: true },
+            { pattern: require.resolve('jasmine-jquery/lib/jasmine-jquery.js'), included: true },
+            { pattern: require.resolve('underscore/underscore.js'), included: true },
+            { pattern: 'static/js/vendor/codemirror-compressed.js', included: true },
+
+            { pattern: path.join(uiToolkitRoot, 'src/js/utils/global-loader.js'), included: true },
+            { pattern: path.join(uiToolkitRoot, 'src/js/utils/string-utils.js'), included: true },
+            { pattern: path.join(uiToolkitRoot, 'src/js/utils/html-utils.js'), included: true },
+
+            { pattern: 'spec/spec_helpers/ajax_prefix.js', included: true },
+            { pattern: 'spec/spec_helpers/i18n.js', included: true },
+            { pattern: 'spec/spec_helpers/logger.js', included: true },
+            { pattern: 'spec/spec_helpers/accessibility_tools.js', included: true },
+            { pattern: 'spec/spec_helpers/add_ajax_prefix.js', included: true },
+            { pattern: 'spec/spec_helpers/helper.js', included: true },
+
+            { pattern: 'spec/spec_helpers/jasmine-waituntil.js', included: true },
+            { pattern: 'spec/spec_helpers/jasmine-extensions.js', included: true },
+            { pattern: 'spec/spec_helpers/jasmine-imagediff.js', included: true },
+
+            { pattern: 'static/js/xmodule.js', included: true },
+            { pattern: 'static/js/javascript_loader.js', included: true },
+            { pattern: 'static/js/collapsible.js', included: true },
+            { pattern: 'static/js/display.js', included: true },
+            { pattern: 'static/js/imageinput.js', included: true },
+            { pattern: 'static/js/schematic.js', included: true },
+
+            { pattern: 'static/js/capa/**/*.js', included: false },
+
+            { pattern: 'static/js/capa/src/jschannel.js', included: true },
+            { pattern: 'static/js/capa/src/jsinput.js', included: true },
+            { pattern: 'static/js/capa/src/formula_equation_preview.js', included: true },
+            { pattern: 'static/js/capa/symbolic_mathjax_preprocessor.js', included: true },
+            { pattern: 'static/js/capa/annotationinput.js', included: true },
+            { pattern: 'static/js/capa/choicetextinput.js', included: true },
+
+            { pattern: 'fixtures/**/*.html', included: false, served: true },
+            { pattern: 'fixtures/**/*.underscore', included: false, served: true },
+            { pattern: 'static/js/capa/fixtures/**/*.html', included: false, served: true },
+            { pattern: 'static/js/capa/genex/**/*', included: false, served: true },
+            { pattern: 'static/js/capa/protex/**/*', included: false, served: true },
+
+
+            { pattern: 'spec/*_spec.js', included: true },
+            { pattern: 'static/js/capa/spec/*_spec.js', included: true },
+
+            { pattern: 'karma_runner.js', included: true }
+        ],
+
+        exclude: [
+            '**/public/**',
+            'static/js/capa/jsinput/jsinput_example.js'
+        ],
+
+        proxies: {
+            '/spec/javascripts/fixtures/': '/base/fixtures/',
+            '/base/fixtures/js/': '/base/static/js/',
+            '/static/js/capa/genex/': '/base/static/js/capa/genex/',
+            '/static/js/capa/protex/': '/base/static/js/capa/protex/'
+        },
+
+        preprocessors: {
+            'static/js/**/*.js': ['sourcemap'],
+            'spec/**/*.js': ['sourcemap']
+        },
+
+        plugins: [
+            'karma-jasmine',
+            'karma-requirejs',
+            'karma-firefox-launcher',
+            'karma-sourcemap-loader',
+            'karma-coverage',
+            require('karma-spec-reporter')
+        ],
+
+        reporters: ['spec', 'coverage'],
+
+        coverageReporter: {
+            dir: 'coverage/',
+            reporters: [
+                { type: 'html', subdir: 'report-html' },
+                { type: 'lcov', subdir: 'report-lcov' }
+            ]
+        },
+
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['FirefoxNoUpdates'],
+
+        customLaunchers: {
+            FirefoxNoUpdates: {
+                base: 'Firefox',
+                prefs: {
+                    'app.update.auto': false,
+                    'app.update.enabled': false
+                }
+            }
+        },
+
+        singleRun: true,
+        concurrency: Infinity,
+        client: { captureConsole: true }
+    });
+};
---
+++ xblocks_contrib/problem/assets/webpack.config.js
@@ -0,0 +1,186 @@
+/* eslint-env node */
+var path = require('path');
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var defineHeader = /\(function ?\(((define|require|requirejs|\$)(, )?)+\) ?\{/;
+var defineCallFooter = /\}\)\.call\(this, ((define|require)( \|\| RequireJS\.(define|require))?(, )?)+?\);/;
+var defineDirectFooter = /\}\(((window\.)?(RequireJS\.)?(requirejs|define|require|jQuery)(, )?)+\)\);/;
+var defineFancyFooter = /\}\).call\(\s*this(\s|.)*define(\s|.)*\);/;
+var defineFooter = new RegExp('(' + defineCallFooter.source + ')|('
+                             + defineDirectFooter.source + ')|('
+                             + defineFancyFooter.source + ')', 'm');
+
+module.exports = {
+    context: __dirname,
+
+    entry: {
+        ProblemBlockDisplay: [
+            './static/js/xmodule.js',
+            './static/js/javascript_loader.js',
+            './static/js/display.js',
+            './static/js/collapsible.js',
+            './static/js/imageinput.js',
+            './static/js/schematic.js'
+        ]
+    },
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '../public/js'),
+        library: {
+            type: 'window'
+        }
+    },
+
+    externals: {
+        jquery: 'jQuery',
+        underscore: '_',
+        $: 'jQuery',
+        gettext: 'gettext'
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        alias: {
+            'codemirror': path.resolve(__dirname, './static/js/vendor/codemirror-compressed.js'),
+            'edx-ui-toolkit': path.resolve(__dirname, '../../../node_modules/edx-ui-toolkit/src/')
+        }
+    },
+
+    plugins: [
+        new webpack.ProvidePlugin({
+            _: 'underscore',
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            CodeMirror: 'codemirror',
+            'edx.HtmlUtils': 'edx-ui-toolkit/js/utils/html-utils'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './static/css/ProblemBlockDisplay.css',
+                    to: path.resolve(__dirname, '../public/css')
+                }
+            ]
+        })
+    ],
+
+    module: {
+        noParse: [
+                /\/codemirror-compressed\.js/
+            ],
+        rules: [
+            {
+                test: /\/static\/js\//,
+                loader: 'string-replace-loader',
+                options: {
+                    multiple: [
+                        { search: defineHeader, replace: '' },
+                        { search: defineFooter, replace: '' },
+                        {
+                            search: /(\/\* RequireJS) \*\//g,
+                            replace(match, p1, offset, string) {
+                                return p1;
+                            }
+                        },
+                        {
+                            search: /\/\* Webpack/g,
+                            replace(match, p1, offset, string) {
+                                return match + ' */';
+                            }
+                        },
+                        {
+                            search: /text!(.*?\.underscore)/g,
+                            replace(match, p1, offset, string) {
+                                return p1;
+                            }
+                        },
+                        { search: /RequireJS.require/g, replace: 'require' }
+                    ]
+                }
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [
+                    /node_modules/,
+                    /\/static\/js\//
+                ],
+                use: 'babel-loader'
+            },
+            {
+                test: /static\/js\/display.js/,
+                use: [
+                    {
+                        loader: 'imports-loader',
+                        options: 'this=>window'
+                    }
+                ]
+            },
+            {
+                test: /static\/js\/imageinput.js/,
+                use: [
+                    {
+                        loader: 'imports-loader',
+                        options: 'this=>window'
+                    }
+                ]
+            },
+            {
+                test: /static\/js\/schematic.js/,
+                use: [
+                    {
+                        loader: 'imports-loader',
+                        options: 'this=>window'
+                    }
+                ]
+            },
+            {
+                test: /static\/js\/collapsible.js/,
+                use: [
+                    {
+                        loader: 'imports-loader',
+                        options: 'this=>window'
+                    }
+                ]
+            },
+            {
+                test: /static\/js\/javascript_loader.js/,
+                use: [
+                    {
+                        loader: 'imports-loader',
+                        options: 'this=>window'
+                    }
+                ]
+            },
+            {
+                test: /static\/js\/xmodule.js/,
+                use: [
+                    {
+                        loader: 'exports-loader',
+                        options: 'window.XModule'
+                    },
+                    {
+                        loader: 'imports-loader',
+                        options: 'this=>window'
+                    }
+                ]
+            },
+            {
+                test: /codemirror/,
+                use: [
+                    {
+                        loader: 'exports-loader',
+                        options: 'window.CodeMirror'
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimize: false
+    },
+    mode: 'development',
+    devtool: 'source-map'
+};
