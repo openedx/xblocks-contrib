37ba434013824d47bfeb1377bea32df827119c94
farhan <farhan.khan@arbisoft.com>
2026-02-17 17:08:44 +0500

    chore: clean the code

--- xblocks_contrib/common/utils.py
+++ xblocks_contrib/common/utils.py
@@ -5,25 +5,25 @@ Common utility functions for XBlocks.
 from django.conf import settings
 
 
-def get_static_resource_path(pipeline_path, debug_path):
+def get_static_resource_path(pipeline_path, dev_path):
     """
     Return the appropriate static resource path for the current environment.
 
     When the Django pipeline is enabled or REQUIRE_DEBUG is False, returns
     pipeline_path (static asset path, e.g. CDN in production). Otherwise returns
-    debug_path (local unpacked asset path).
+    dev_path (local unpacked asset path).
 
     For more details, see platform's xblock_local_resource_url() in:
     https://github.com/openedx/openedx-platform/blob/master/openedx/core/lib/xblock_utils/__init__.py
 
     Arguments:
         pipeline_path (str): Path used when pipeline is enabled or not in debug.
-        debug_path (str): Path used in local debug (REQUIRE_DEBUG=True, pipeline off).
+        dev_path (str): Path used in dev/debug mode of runtime/openedx-platform
 
     Returns:
         str: The path to use for local_resource_url().
     """
     pipeline = getattr(settings, 'PIPELINE', {})
-    if pipeline.get('PIPELINE_ENABLED', False) or not getattr(settings, 'REQUIRE_DEBUG', False):
+    if pipeline.get('PIPELINE_ENABLED', True) or not getattr(settings, 'REQUIRE_DEBUG', False):
         return pipeline_path
-    return debug_path
+    return dev_path
