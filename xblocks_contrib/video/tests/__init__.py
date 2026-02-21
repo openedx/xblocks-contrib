ae0c7c66766082d159f3d7de13367e491b8dea8b
farhan <farhan.khan@arbisoft.com>
2026-02-21 12:44:58 +0500

    chore: quality fixes

--- xblocks_contrib/video/tests/__init__.py
+++ xblocks_contrib/video/tests/__init__.py
@@ -3,7 +3,7 @@ Tests for the video xblock.
 """
 
 from unittest.mock import Mock
-from xblocks_contrib.video.video import VideoBlock
 
+from xblocks_contrib.video.video import VideoBlock
 
 VideoBlock.add_aside = Mock()
